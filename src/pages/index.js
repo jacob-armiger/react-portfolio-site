79d16c80e6eece1885e1815fd3269dfb975681ba
jacob-armiger <armigersemail@gmail.com>
2023-04-19 22:45:24 -0400

    Update index design

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,38 +7,28 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center">
-      <Image
-        src="/profile.jpg"
-        alt="Picture of Jacob"
-        quality={100}
-        width={360}
-        height={545}
-      />
+    <div className="flex flex-col items-center md:flex-row">
+      <div className="h-10/12 mx-6 mt-12 flex flex-col justify-center sm:ml-24 sm:mt-0 sm:h-screen sm:pb-48">
+        <div className="pb-8">
+          <p className="text-7xl sm:text-9xl">Hello,</p>
+          <p className="text-7xl sm:text-9xl">I'm Jacob</p>
+        </div>
 
-      <div className="max-w-prose text-center">
-        <h1>Jacob Armiger</h1>
-
-        <i>Welcome to my portfolio!</i>
-
-        <p>
-          I'm a special operations veteran pursuing a career in computer
-          science. Looking to make meaningful changes in the lives of others in
-          the world's most rapidly evolving industry.
-        </p>
-
-        <div className="space-x-5">
-          <a target="_blank" href="https://github.com/jacob-armiger">
-            Github
-          </a>
-          <a
-            target="_blank"
-            href="https://www.linkedin.com/in/jacob-armiger-64189317a/"
-          >
-            LinkedIn
-          </a>
+        <div className="max-w-sm">
+          <p className="text-left">
+            I'm a special operations veteran pursuing a career in the world's
+            most rapidly evolving industry.
+          </p>
         </div>
       </div>
+
+      <a
+        className="mb-24 mt-16 h-14 w-32 rounded-full border border-black pt-3.5 text-center hover:bg-gray-400 sm:m-auto"
+        target="_blank"
+        href="https://github.com/jacob-armiger"
+      >
+        Github
+      </a>
     </div>
   );
 }
