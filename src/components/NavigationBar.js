e85dae133eecec55270ecbdcc5a65a2ad911081c
jacob-armiger <armigersemail@gmail.com>
2023-05-14 00:26:00 -0400

    Update styles

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -5,13 +5,13 @@ export default function NavBar() {
     <div className="mx-8 mt-6 text-xl md:mx-48 lg:mx-64 xl:mx-96">
       <nav className="flex items-center justify-between">
         <div>
-          <Link href="/">Home</Link>
+          <Link className="nav-link" href="/">Home</Link>
         </div>
         <div>
-          <Link href="/resume">Resume</Link>
+          <Link className="nav-link" href="/resume">Resume</Link>
         </div>
         <div>
-          <Link href="/projects">Projects</Link>
+          <Link className="nav-link" href="/projects">Projects</Link>
         </div>
       </nav>
     </div>
