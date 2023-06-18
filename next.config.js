c11d06b7d9553bd0f4638e937f2b7f1dfb72550e
jacob-armiger <armigersemail@gmail.com>
2023-06-18 17:42:02 -0500

    Add nextjs rewrite

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: "http://127.0.0.1:8000/api/:path*",
+            },
+        ]
+    },
+    reactStrictMode: true,
+};
