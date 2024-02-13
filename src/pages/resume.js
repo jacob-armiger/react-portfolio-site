298cd2902b27c68230095b3a61286b4d61a7e0ea
jacob-armiger <armigersemail@gmail.com>
2024-02-12 22:52:09 -0500

    Update resume file

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -4,7 +4,7 @@ export default function Resume() {
       <div className="flex h-screen w-auto flex-col-reverse items-center sm:ml-4 sm:flex-row">
         <iframe
           className="h-5/6 w-11/12 rounded-lg border border-b-4 border-black md:w-8/12"
-          src="/master_redacted_resume.pdf"
+          src="/resume.pdf"
         />
         <p className="mb-14 ml-4 mt-8 sm:mx-4 sm:mb-16">
           This is my resume. I've redacted some personal information.
