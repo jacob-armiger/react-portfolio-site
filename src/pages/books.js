c6fc86f3bf96048688b478bea9c76f163d527f35
jacob-armiger <armigersemail@gmail.com>
2024-02-16 17:44:05 -0500

    Update styling with masonry cards

--- src/pages/books.js
+++ src/pages/books.js
@@ -4,22 +4,26 @@ import { useState } from 'react';
 import Book from "@/components/Book";
 
 export default function Books() {
-    const [books, setBooks] = useState([])
- 
-    useEffect(() => {
-        fetchBooks()
-    }, [])
+    const [books, setBooks] = useState([]);
 
+    useEffect(() => {
+        fetchBooks();
+    }, []);
 
     let fetchBooks = async () => {
-        let data = await supabase.from("book_data").select().eq('Exclusive Shelf', 'read');
-        console.log(data.data)
-        setBooks(data.data)
-    }
+        let data = await supabase
+            .from("book_data2")
+            .select()
+            .eq("Exclusive Shelf", "read");
+        console.log(data.data);
+        setBooks(data.data);
+    };
 
     return (
-        <div className="my-12 flex flex-col items-left gap-5">
-            {books?.map(book => <Book key={book.Title} book={book}/>)}
+        <div className="mx-6 my-12 columns-2 md:columns-3 lg:columns-4">
+            {books?.map((book) => (
+                <Book key={book.Title} book={book} />
+            ))}
         </div>
     );
 }
