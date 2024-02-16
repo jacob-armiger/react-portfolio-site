7d08c1c8b02e74ab940ff12e5fc8ec314f712432
jacob-armiger <armigersemail@gmail.com>
2024-02-16 15:35:11 -0500

    Add file for supabase client creation

--- src/pages/books.js
+++ src/pages/books.js
@@ -1,25 +1,20 @@
-import { Inter } from "next/font/google";
-const inter = Inter({ subsets: ["latin"] });
-
-import { createClient } from '@supabase/supabase-js'
+import { supabase } from "@/utils/supabase";
 import { useEffect } from "react";
 import { useState } from 'react';
 
-const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
-
 export default function Books() {
     const [books, setBooks] = useState([])
+ 
+    useEffect(() => {
+        fetchBooks()
+    }, [])
 
 
     let fetchBooks = async () => {
         let data = await supabase.from("book_data").select().eq('Exclusive Shelf', 'read');
         console.log(data.data)
         setBooks(data.data)
-     }
- 
-     useEffect(() => {
-        fetchBooks()
-     }, [books])
+    }
 
     return (
         <div className="my-12 flex flex-col items-left gap-5">
