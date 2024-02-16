fd7d93406d83cda0e015ec90b89279a3021b1b15
jacob-armiger <armigersemail@gmail.com>
2024-02-16 14:58:14 -0500

    Update env name

--- src/pages/books.js
+++ src/pages/books.js
@@ -5,7 +5,7 @@ import { createClient } from '@supabase/supabase-js'
 import { useEffect } from "react";
 import { useState } from 'react';
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY)
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
 
 export default function Books() {
     const [books, setBooks] = useState([])
