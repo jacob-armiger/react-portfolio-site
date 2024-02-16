1e8a9f3a4c4c778b2ec93f8643ebd67a4af5ceb1
jacob-armiger <armigersemail@gmail.com>
2024-02-16 17:43:12 -0500

    Add rating and review to Book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,8 +1,25 @@
 export default function Book({ book }) {
     return (
-        <div className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{book.Title}</h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">{book.Author}</p>
+        <div className="mb-6 block max-w-lg break-inside-avoid-column rounded-lg border border-gray-200 bg-white p-6 shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <h5 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                {book.Title}
+            </h5>
+            <p className="font-normal text-gray-500 dark:text-gray-400">
+                {book.Author}
+            </p>
+            <div className="flex">
+                <svg
+                    className="me-1 h-4 w-4 text-yellow-300"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 22 20"
+                >
+                    <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
+                </svg>
+                <p>{book["My Rating"]}</p>
+            </div>
+            <p>{book["My Review"]}</p>
         </div>
     );
 }
