d491e358c5934bfd9dd87fd6ac51e862361af58f
jacob-armiger <armigersemail@gmail.com>
2024-02-20 14:01:08 -0500

    Update resume page responsiveness

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -2,7 +2,7 @@ export default function Resume() {
     return (
         <div className="flex h-screen w-auto flex-col-reverse items-center sm:ml-4 lg:flex-row">
             <iframe
-                className="h-5/6 w-11/12 rounded-lg border border-b-4 border-black md:w-8/12"
+                className="h-5/6 w-11/12 rounded-lg border border-b-4 border-black lg:w-8/12"
                 src="/resume.pdf"
             />
             <p className="mb-14 ml-4 mt-8 sm:mx-4 sm:mb-16">
