ef65230ec100a4c71b486b30ec81564f175148dd
jacob-armiger <armigersemail@gmail.com>
2024-02-20 15:10:11 -0500

    Add content

--- src/pages/project/DevBlog.js
+++ src/pages/project/DevBlog.js
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import Button from "@/components/Button";
 export default function Project() {
     let router = useRouter();
 
@@ -11,16 +12,28 @@ export default function Project() {
         <div className="m-auto w-8/12 content-center">
             <h1 className="my-4 text-center font-bold">{project?.name}</h1>
             <img className="" src={project?.image} />
-            <div className="flex flex-col items-center justify-center sm:flex-row">
-                <a
-                    className="rich-link"
-                    target="_blank"
-                    href="https://github.com/jacob-armiger/django-devblog"
-                >
-                    Github
-                </a>
+            {/* Content */}
+            <div className="mx-1 my-4 flex max-w-prose flex-col items-center sm:mx-auto sm:items-start">
+                <div className="flex w-min flex-row space-x-4">
+                    <Button
+                        url="https://github.com/jacob-armiger/django-devblog"
+                        title="Project Github"
+                    />
+                </div>
+                <p>
+                    This is the first personal project I worked on way back in
+                    2019 when I was in the army. After reading through Python
+                    Crash Course by Eric Matthes, where I learned Python and
+                    Django, I tried making my own custom website. I used Django
+                    to implement comments, admin users, blog creation, and blog
+                    filtering. I also hosted the project through Heroku.
+                    <br />
+                    <br />
+                    Unfortunately, I no longer host it so it cannot be visited,
+                    but feel free to check out my code (at your own risk) at the
+                    Github link.
+                </p>
             </div>
-            <p>*Under Construction*</p>
         </div>
     );
 }
