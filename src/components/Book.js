dd530b06f872e40284ee46c2f5c2e27799314fd8
jacob-armiger <armigersemail@gmail.com>
2024-02-20 12:57:25 -0500

    Update book card styling

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,6 @@
 export default function Book({ book }) {
     return (
-        <div className="mb-6 block max-w-lg break-inside-avoid-column rounded-lg border border-gray-200 bg-white p-6 shadow hover:bg-gray-100 ">{/* dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 */}
+        <div className="mb-6 max-w-lg break-inside-avoid rounded-lg border border-black p-5"> {/* dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 */}
             <h5 className="mb-1 text-2xl font-bold tracking-tight text-gray-900 "> {/* dark:text-white */}
                 {book.Title}
             </h5>
@@ -17,7 +17,7 @@ export default function Book({ book }) {
                 >
                     <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
                 </svg>
-                <p>{book["My Rating"]}</p>
+                <p>{book["My Rating"]}/5</p>
             </div>
             <p>{book["My Review"]}</p>
         </div>
