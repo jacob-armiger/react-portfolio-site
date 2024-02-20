991b83ad882459fbf1f2687d481879da358d73c7
jacob-armiger <armigersemail@gmail.com>
2024-02-20 13:58:34 -0500

    Update order of navbar links

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,22 +1,30 @@
 import Link from "next/link";
 
 export default function NavBar() {
-  return (
-    <div className="mx-8 mt-6 text-xl md:mx-48 lg:mx-64 xl:mx-96">
-      <nav className="flex items-center justify-between">
-        <div>
-          <Link className="nav-link" href="/">Home</Link>
+    return (
+        <div className="mx-8 mt-6 text-xl md:mx-48 lg:mx-64 xl:mx-96">
+            <nav className="flex items-center justify-between">
+                <div>
+                    <Link className="nav-link" href="/">
+                        Home
+                    </Link>
+                </div>
+                <div>
+                    <Link className="nav-link" href="/projects">
+                        Projects
+                    </Link>
+                </div>
+                <div>
+                    <Link className="nav-link" href="/books">
+                        Books
+                    </Link>
+                </div>
+                <div>
+                    <Link className="nav-link" href="/resume">
+                        Resume
+                    </Link>
+                </div>
+            </nav>
         </div>
-        <div>
-          <Link className="nav-link" href="/resume">Resume</Link>
-        </div>
-        <div>
-          <Link className="nav-link" href="/projects">Projects</Link>
-        </div>
-        <div>
-          <Link className="nav-link" href="/books">Books</Link>
-        </div>
-      </nav>
-    </div>
-  );
+    );
 }
