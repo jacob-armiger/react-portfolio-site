bcc85ee2964bab19bb65f24c2449ad45decb9cb7
jacob-armiger <armigersemail@gmail.com>
2024-02-21 13:55:48 -0500

    Update index page styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Button({ length, url = "", title = "Lorem Ipsum" }) {
     return (
         <Link
-            className="h-14 w-40 rounded-3xl border border-b-4 border-black pt-3.5 text-center hover:border hover:bg-gray-400 sm:m-auto"
+            className="h-14 w-40 rounded-3xl border border-b-4 border-black pt-3.5 text-center hover:border hover:bg-gray-400"
             target="_blank"
             href={url}
         >
