0080572646e17b4eadb0b3bc3dc0c4c4b111d521
jacob-armiger <armigersemail@gmail.com>
2024-02-26 21:30:08 -0500

    Add Sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,32 +1,50 @@
-export default function Sidebar({func}) {
+import { useState, useEffect } from "react";
+
+export default function Sidebar({ onSortChange, browsing }) {
+    const [sortType, setSortType] = useState(null);
+
+    const handleClick = (e) => {
+        setSortType(e?.target.value)
+        onSortChange(e?.target.value)
+    };
 
     return (
-        <div></div>
-        // <aside
-        //     className="sticky top-48 h-full -translate-x-full transition-transform sm:left-2 sm:mx-4 sm:translate-x-0"
-        //     aria-label="Sidebar"
-        // >
-        //     <div className="hidden h-64 w-0 rounded-lg border border-black px-3 py-4 sm:visible sm:block sm:w-48">
-        //         <p className="font-bold">Sort By</p>
-        //         <button
-        //             onClick={func}
-        //             className={`rounded-full px-4 py-2 hover:bg-gray-300 ${
-        //                 sortType === "title" ? "border border-black" : null
-        //             }`}
-        //             value="title"
-        //         >
-        //             Title
-        //         </button>
-        //         <button
-        //             onClick={func}
-        //             className={`rounded-full px-4 py-2 hover:bg-gray-300 ${
-        //                 sortType === "stars" ? "border border-black" : null
-        //             }`}
-        //             value="stars"
-        //         >
-        //             Stars
-        //         </button>
-        //     </div>
-        // </aside>
+        <aside
+            aria-label="Sidebar"
+            className={`sticky top-64 -ml-48 h-full transition-all ${
+                browsing ? "sm:ml-4" : "sm:-ml-48"
+            }`}
+        >
+            <div className="h-64 w-48 space-x-1 space-y-1 rounded-lg border border-b-4 border-l-2 border-black px-3 py-4">
+                <p className="font-bold">Sort By</p>
+                <button
+                    onClick={handleClick}
+                    value="title"
+                    className={`rounded-full px-4 py-2 hover:bg-gray-300 ${
+                        sortType === "title" ? "border border-black" : null
+                    }`}
+                >
+                    Title
+                </button>
+                <button
+                    onClick={handleClick}
+                    value="stars"
+                    className={`rounded-full px-4 py-2 hover:bg-gray-300 ${
+                        sortType === "stars" ? "border border-black" : null
+                    }`}
+                >
+                    Rating
+                </button>
+                <button
+                    onClick={handleClick}
+                    value="author"
+                    className={`rounded-full px-4 py-2 hover:bg-gray-300 ${
+                        sortType === "author" ? "border border-black" : null
+                    }`}
+                >
+                    Author
+                </button>
+            </div>
+        </aside>
     );
 }
