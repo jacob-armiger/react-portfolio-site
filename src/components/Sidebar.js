bca527a2a45c14916b5940f368b80e0351dab03c
jacob-armiger <armigersemail@gmail.com>
2024-02-26 23:27:26 -0500

    Add filter checkboxes and handler function

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,6 +3,7 @@ import { compareAuthor, compareTitle, compareStars } from "@/utils/helper";
 
 export default function Sidebar({ updateBooks, browsing, books }) {
     const [sortType, setSortType] = useState(null);
+    const [filters, setFilters] = useState([true, false]);
 
     useEffect(() => {
         // Shallow copy array so state can be updated in parent
@@ -20,9 +21,24 @@ export default function Sidebar({ updateBooks, browsing, books }) {
         updateBooks(sorted_books);
     }, [sortType]);
 
-    const handleClick = (e) => {
+    const handleSortClick = (e) => {
         setSortType(e?.target.value);
     };
+
+    const handleFilterClick = (e) => {
+        let box = e.target.value;
+
+        if (box == "read") {
+            // Logic to keep at least one box checked
+            if ((filters[0] && filters[1]) || filters[1]) {
+                setFilters([!filters[0], filters[1]]);
+            }
+        } else {
+            if ((filters[0] && filters[1]) || filters[0]) {
+                setFilters([filters[0], !filters[1]]);
+            }
+        }
+    }
 
     return (
         <aside
@@ -31,35 +47,59 @@ export default function Sidebar({ updateBooks, browsing, books }) {
                 browsing ? "sm:ml-4" : "sm:-ml-48"
             }`}
         >
-            <div className="h-64 w-48 space-x-1 space-y-1 rounded-lg border border-b-4 border-l-2 border-black px-3 py-4">
-                <p className="font-bold">Sort By</p>
-                <button
-                    onClick={handleClick}
-                    value="title"
-                    className={`rounded-full px-4 py-2 hover:bg-gray-300 ${
-                        sortType === "title" ? "border border-black" : null
-                    }`}
-                >
-                    Title
-                </button>
-                <button
-                    onClick={handleClick}
-                    value="stars"
-                    className={`rounded-full px-4 py-2 hover:bg-gray-300 ${
-                        sortType === "stars" ? "border border-black" : null
-                    }`}
-                >
-                    Rating
-                </button>
-                <button
-                    onClick={handleClick}
-                    value="author"
-                    className={`rounded-full px-4 py-2 hover:bg-gray-300 ${
-                        sortType === "author" ? "border border-black" : null
-                    }`}
-                >
-                    Author
-                </button>
+            <div className="h-64 w-48  rounded-lg border border-b-4 border-l-2 border-black px-3 py-4">
+                <div className="space-x-1 space-y-1">
+                    <p className="font-bold">Sort By</p>
+                    <button
+                        onClick={handleSortClick}
+                        value="title"
+                        className={`rounded-md px-4 py-2 hover:bg-gray-300 ${
+                            sortType === "title" ? "border border-black" : null
+                        }`}
+                    >
+                        Title
+                    </button>
+                    <button
+                        onClick={handleSortClick}
+                        value="stars"
+                        className={`rounded-md px-4 py-2 hover:bg-gray-300 ${
+                            sortType === "stars" ? "border border-black" : null
+                        }`}
+                    >
+                        Rating
+                    </button>
+                    <button
+                        onClick={handleSortClick}
+                        value="author"
+                        className={`rounded-md px-4 py-2 hover:bg-gray-300 ${
+                            sortType === "author" ? "border border-black" : null
+                        }`}
+                    >
+                        Author
+                    </button>
+                </div>
+                <p className="font-bold">Filter By</p>
+                <div className="flex flex-col">
+                    <div className="flex flex-row items-center space-x-1">
+                        <input
+                            type="checkbox"
+                            value="read"
+                            checked={filters[0]}
+                            onChange={handleFilterClick}
+                        />
+                        <label className="">Read</label>
+                    </div>
+
+                    <div className="flex flex-row items-center space-x-1">
+                        <input
+                            type="checkbox"
+                            value="want-to-read"
+                            checked={filters[1]}
+                            onChange={handleFilterClick}
+                        />
+                        <label>Want to Read</label>
+                    </div>
+                </div>
             </div>
         </aside>
     );
