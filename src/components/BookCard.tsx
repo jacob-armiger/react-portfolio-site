c25772425ebdc1d9cd69cb187013ca63f36a9f14
jacob-armiger <armigersemail@gmail.com>
2024-10-27 23:00:07 -0400

    Remove import

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -1,13 +1,11 @@
-import react, { useState } from "react"
+import react from "react"
 
 export default function BookCard({ book }: any) {
 
-    const [expand, setExpand] = useState(false);
-
     return (
         <div
-            onClick={() => {setExpand(!expand)}}
-            className={`${expand && book["My Review"] ? "row-span-2 max-h-[20.75rem] overflow-y-scroll" : "h-36"} ${book["My Review"] ? "hover:cursor-pointer" : ""} grid border border-solid border-black text-xl p-4 m-5 rounded-md transition-all duration-600 overflow-y-clip `}
+            className={`${book["My Review"] ? "row-span-2 max-h-[20.75rem] overflow-y-scroll" : "h-36"}
+                        grid cursor-default border border-solid border-black text-xl p-4 m-5 rounded-md overflow-y-clip `}
         >
             <div>
                 <p className="font-bold">{book.Title}</p>
