d89cda95ac28c57ccd7c20f039c1b5d061ec70e4
jacob-armiger <armigersemail@gmail.com>
2024-10-31 22:17:05 -0400

    Configure output to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind(), react()],
-    output: 'server',
+    output: 'static',
     adapter: vercel({
         imageService: true,
     }),
