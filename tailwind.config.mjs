1c914eef70f236af3c89d92eb9c77b6c2c3efbcf
jacob-armiger <armigersemail@gmail.com>
2024-11-17 17:35:28 -0500

    Add custom tailwind colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+
+        colors: {
+            // 'custom': '#5a6f91',
+            // 'custom': '#faf9f6',
+            'custom': '#E2E6E7',
+            // 'fade': '#373737',
+            'fade': '#3b3b3b',
+            // 'accent': 'white',
+        },
+        },
 	},
 	plugins: [
         require('@tailwindcss/typography'),
