a9a20e72a523c1a46414d7b4f815f347f4c8679c
jacob-armiger <armigersemail@gmail.com>
2024-11-28 18:42:25 -0600

    feat: add text preview to art
    Add ArtPreview component. Add types to art collection. Add Title.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,5 +1,5 @@
 // 1. Import utilities from `astro:content`
-import { z, defineCollection } from 'astro:content';
+import { z, defineCollection, reference } from 'astro:content';
 
 // 2. Define a `type` and `schema` for each collection
 const artCollection = defineCollection({
@@ -7,6 +7,9 @@ const artCollection = defineCollection({
   schema: ({ image }) => z.object({
     date: z.date(),
     image: image(),
+    title: z.string().optional(),
+    reference: z.string().optional(),
+    reference_url: z.string().url().optional(),
     // img: z.string(),
     // tags: z.array(z.string()),
     // image: z.string().optional(),
