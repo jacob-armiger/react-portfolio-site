425fc9f190a1f523205205e19952bacbc7726b70
jacob-armiger <armigersemail@gmail.com>
2026-02-28 15:46:00 -0500

    Add image optimizations for art and update article images to be smaller

--- src/components/ArtMasonryGrid.tsx
+++ src/components/ArtMasonryGrid.tsx
@@ -1,15 +1,29 @@
-import { Image } from "astro:assets";
 import { RegularMasonryGrid as MasonryGrid, Frame } from '@masonry-grid/react'
 
-export default function ArtMasonryGrid({ artEntries }: { artEntries: ImageMetadata[] }) {
-    // https://www.reddit.com/r/astrojs/comments/1bia6lq/how_to_utilize_image_with_react_component/
+interface OptimizedImage {
+    src: string;
+    width: number;
+    height: number;
+    originalSrc: string;
+}
+
+export default function ArtMasonryGrid({ artEntries }: { artEntries: OptimizedImage[] }) {
     return (
-        <MasonryGrid frameWidth={280} gap={10} >
-                {artEntries.map((img: ImageMetadata) => (
-                    <Frame width={img.width} height={img.height}>
-                        <img src={img.src} alt="Artwork" className="zoomable" />
-                    </Frame>
-                ))}
+        <MasonryGrid frameWidth={280} gap={10}>
+            {artEntries.map((img, i) => (
+                <Frame key={i} width={img.width} height={img.height}>
+                    <img
+                        src={img.src}
+                        alt="Artwork"
+                        className="zoomable"
+                        loading="lazy"
+                        width={img.width}
+                        height={img.height}
+                        data-zoom-src={img.originalSrc}
+                        style={{ cursor: 'zoom-in' }}
+                    />
+                </Frame>
+            ))}
         </MasonryGrid>
     );
 }
