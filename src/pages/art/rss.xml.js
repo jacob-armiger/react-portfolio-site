ff578c5087e112430380d5497e45ef3a67213346
Jacob <armigersemail@gmail.com>
2026-07-08 18:53:18 -0400

    Update content and add new RSS feed (#26)
    * Add content and update image ordering
    
    * Add combined RSS feed

--- src/pages/art/rss.xml.js
+++ src/pages/art/rss.xml.js
@@ -17,7 +17,6 @@ const toDisplayName = (filename) =>
 
 export async function GET(context) {
     const artImages = import.meta.glob([
-        '../../assets/selectedWork/*.{png,jpg,jpeg}',
         '../../assets/drawings/*.{png,jpg,jpeg}',
         '../../assets/oilPaintings/*.{png,jpg,jpeg}',
         '../../assets/digitalStudies/*.{png,jpg,jpeg}',
